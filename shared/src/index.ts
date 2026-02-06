a99cdd2a456b895b1ed60ca49d73f497bdc31c00
ginhooser-cyber <ginhooser@gmail.com>
2026-02-06 13:51:52 +0000

    Initialize Lineremain project skeleton with monorepo structure

---
+++ shared/src/index.ts
@@ -0,0 +1,20 @@
+// Lineremain — Shared Types & Constants
+// This package is used by both client and server
+
+export * from './types/entities';
+export * from './types/items';
+export * from './types/blocks';
+export * from './types/network';
+export * from './types/player';
+export * from './types/recipes';
+export * from './types/buildings';
+export * from './types/biomes';
+export * from './constants/game';
+export * from './constants/items';
+export * from './constants/blocks';
+export * from './constants/recipes';
+export * from './constants/buildings';
+export * from './constants/combat';
+export * from './constants/survival';
+export * from './utils/chunkUtils';
+export * from './utils/inventoryUtils';
